8b1ccb7a741c78145620683cf4063c921dc6890a
roni shahar <ronishahar21@gmail.com>
2022-01-19 12:44:11 +0200

    Entity and DB classes

---
+++ src/main/java/workspace/createDB/DB.java
@@ -0,0 +1,16 @@
+package workspace.createDB;
+
+import java.util.List;
+
+public class DB {
+
+    List<Entity> dataBase;
+
+    public List<Entity> getDataBase() {
+        return dataBase;
+    }
+
+    public void setDataBase(List<Entity> dataBase) {
+        this.dataBase = dataBase;
+    }
+}
---
+++ src/main/java/workspace/createDB/Entity.java
@@ -0,0 +1,16 @@
+package workspace.createDB;
+
+import java.util.List;
+
+public class Entity {
+
+    List<Records> entities;
+
+    public List<Records> getEntities() {
+        return entities;
+    }
+
+    public void setEntities(List<Records> entities) {
+        this.entities = entities;
+    }
+}
